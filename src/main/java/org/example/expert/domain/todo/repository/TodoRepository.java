090addb1da3dd170971aae13695c42084310f591
tmdcksdl <tmdcksdl99@naver.com>
2025-01-20 10:15:39 +0900

    refactor: JPQL 줄바꿈 및 코드 정리
    - JPQL 쿼리를 줄바꿈하여 가독성 향상

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -12,22 +12,32 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t WHERE t.weather = :weather ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t " +
+            "WHERE t.weather = :weather " +
+            "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByWeatherOrderByModifiedAtDesc(@Param("weather") String weather, Pageable pageable);
 
-    @Query("SELECT t FROM Todo t WHERE t.modifiedAt BETWEEN :startDateTime AND :endDateTime ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t " +
+            "WHERE t.modifiedAt BETWEEN :startDateTime AND :endDateTime " +
+            "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByModifiedAtBetween(
             @Param("startDateTime") LocalDateTime startDateTime,
             @Param("endDateTime")LocalDateTime endDateTime,
             Pageable pageable);
 
-    @Query("SELECT t FROM Todo t WHERE t.modifiedAt >= :startDateTime ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t " +
+            "WHERE t.modifiedAt >= :startDateTime " +
+            "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByModifiedAtAfter(@Param("startDateTime") LocalDateTime startDateTime, Pageable pageable);
 
-    @Query("SELECT t FROM Todo t WHERE t.modifiedAt <= :endDateTime ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t " +
+            "WHERE t.modifiedAt <= :endDateTime " +
+            "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByModifiedAtBefore(@Param("endDateTime") LocalDateTime endDateTime, Pageable pageable);
 
 
